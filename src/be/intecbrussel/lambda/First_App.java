f59a60ec89cac6aaab3bf268f6b1b502a84ec5c2
intec <intec@intec.intecbrussel.be>
2018-08-23 10:28:41 +0200

    updated

--- src/be/intecbrussel/lambda/First_App.java
+++ src/be/intecbrussel/lambda/First_App.java
@@ -1,5 +1,10 @@
 package be.intecbrussel.lambda;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class First_App {
 
 	public static void main(String[] args) {
@@ -8,6 +13,19 @@ public class First_App {
 		 
 				System.out.println("with Lambda Expression");
 				runLambda.run();
+				
+				
+				Set test = new HashSet();
+				test.add("test1");
+				test.add("test2");
+				test.add("test3");
+				test.add("test4");
+				
+				String[] strArray = {"Atilla","Tarik","Jamie","Jason","Mulham"};
+				
+				List characters = Arrays.asList(strArray);
+				
+				characters.forEach((character) -> System.out.println(character));				
 
 	}
 
